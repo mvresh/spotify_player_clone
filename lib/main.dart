82589563381443c552486c48f472bbda0a203b2d
Varun Resh Maddali <mvresh@gmail.com>
2019-10-07 12:40:36 +0530

    done spotify stretch

--- lib/main.dart
+++ lib/main.dart
@@ -27,10 +27,98 @@ class _SpotifyPlayerState extends State<SpotifyPlayer> {
       'https://p.scdn.co/mp3-preview/4efd033217aa13f4625d37f95efa676fb02d4778?cid=774b29d4f13844c495f206cafdad9c86',
       'https://i.scdn.co/image/f218335b215402cc2fb3b8d92652ebad48458805',
       'Luka Chuppi');
-  List<Song> allSongs = SongData().songs; // You are given a list of songs here for Stretch
+  List<Song> allSongs = SongData().songs;// You are given a list of songs here for Stretch
+  IconData buttonState = Icons.play_circle_filled;
+  AudioPlayer audioPlayer = AudioPlayer();
+  int i = 0;
+  playPrev(){
+    setState(() {
+      if(i!=0){
+        i--;
+        if(buttonState != Icons.play_circle_filled){
+          audioPlayer.play(allSongs[i].playUrl);
+        }
+      }
+    });
+  }
+  playNext(){
+    setState(() {
+      if(i!=allSongs.length){
+        i++;
+        if(buttonState != Icons.play_circle_filled){
+          audioPlayer.play(allSongs[i].playUrl);
+        }
+      }
+    });
+  }
+
+  changeButton() {
+    setState(() {
+      if (buttonState == Icons.play_circle_filled) {
+        audioPlayer.play(allSongs[i].playUrl);
+        buttonState = Icons.pause_circle_filled;
+      } else if (buttonState == Icons.pause_circle_filled) {
+        audioPlayer.pause();
+        buttonState = Icons.play_circle_filled;
+      }
+
+      return buttonState;
+    });
+  }
+
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            Image.network(
+              '${allSongs[i].imageUrl}',
+            ),
+            SizedBox(height: 20),
+            Text('${allSongs[i].name}',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  color: Colors.black,
+                )),
+            Text('${allSongs[i].artistName}',
+                style: TextStyle(
+                  fontStyle: FontStyle.italic,
+                  fontSize: 10,
+                  color: Colors.black,
+                )),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                IconButton(
+                    onPressed: () {}, iconSize: 36, icon: Icon(Icons.thumb_up)),
+                IconButton(
+                    onPressed: playPrev,
+                    color: Colors.black,
+                    iconSize: 36,
+                    icon: Icon(Icons.skip_previous)),
+                IconButton(
+                  iconSize: 64,
+                  icon: Icon(buttonState),
+                  onPressed: changeButton,
+                ),
+                IconButton(
+                    onPressed: playNext,
+                    iconSize: 36,
+                    icon: Icon(Icons.skip_next)),
+                IconButton(
+                    onPressed: () {},
+                    iconSize: 36,
+                    icon: Icon(Icons.thumb_down)),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
